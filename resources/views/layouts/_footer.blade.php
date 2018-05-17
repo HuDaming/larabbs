c010fe3a13117621df2b85f9fef7af616107c394
Hudaming <40834106@qq.com>
2018-05-17 10:12:04 +0000

    Admin can manage site settings

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,6 +4,6 @@
             由 <a href="https://user.qzone.qq.com/40834106/infocenter?_t_=0.2866964753471508" target="_blank">Apple情绪化</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
-        <p class="pull-right"><a href="mailto:40834106@qq.com">联系我们</a></p>
+        <p class="pull-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
     </div>
 </footer>
